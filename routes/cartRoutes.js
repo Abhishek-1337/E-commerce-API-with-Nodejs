c466acc41a57f42630e00bf3cb0461052fc2403a
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-08-18 21:23:08 +0530

    routes updated

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,5 +4,9 @@ const cartController = require("../controllers/cartController");
 
 router.get("/", cartController.viewCart);
 router.route("/add-to-cart").post(cartController.addItem);
+router
+  .route("/:id")
+  .delete(cartController.deleteProduct)
+  .patch(cartController.updateProduct);
 
 module.exports = router;
